62c80d94ff8a88c835fc936c941b6a8df98d6e1a
Atul Singh <cocatul11@gmail.com>
2024-08-06 20:48:14 +0530

    feat: Add TransformerCodeAlreadyExistsException class

---
+++ Street_light/src/main/java/ashis/com/backend/Street_light/exceptions/TransformerCodeAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package ashis.com.backend.Street_light.exceptions;
+
+public class TransformerCodeAlreadyExistsException extends RuntimeException{
+    public TransformerCodeAlreadyExistsException(String message) {
+        super(message);
+    }
+}
