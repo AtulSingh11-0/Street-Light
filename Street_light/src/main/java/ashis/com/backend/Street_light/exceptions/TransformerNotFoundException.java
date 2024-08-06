9eadcc47fcaea20e4689a229cc50853f44212dbf
Atul Singh <cocatul11@gmail.com>
2024-08-06 20:48:09 +0530

    feat: Add TransformerNotFoundException class

---
+++ Street_light/src/main/java/ashis/com/backend/Street_light/exceptions/TransformerNotFoundException.java
@@ -0,0 +1,7 @@
+package ashis.com.backend.Street_light.exceptions;
+
+public class TransformerNotFoundException extends RuntimeException{
+    public TransformerNotFoundException(String message) {
+        super(message);
+    }
+}
