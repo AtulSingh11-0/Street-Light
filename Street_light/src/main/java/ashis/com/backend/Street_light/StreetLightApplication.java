8afb86609f909c6eb432ba9f4d46c7f500524d28
Atul Singh <cocatul11@gmail.com>
2024-08-06 20:47:01 +0530

    chore: Update Maven and MySQL configurations

---
+++ Street_light/src/main/java/ashis/com/backend/Street_light/StreetLightApplication.java
@@ -0,0 +1,14 @@
+package ashis.com.backend.Street_light;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreetLightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreetLightApplication.class, args);
+		System.out.println("Street Light Application Started");
+	}
+
+}
---
+++ Street_light/src/test/java/ashis/com/backend/Street_light/StreetLightApplicationTests.java
@@ -0,0 +1,13 @@
+package ashis.com.backend.Street_light;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreetLightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
