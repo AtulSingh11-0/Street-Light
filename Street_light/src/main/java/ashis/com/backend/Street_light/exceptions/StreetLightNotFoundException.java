d7045c810e5ed78e1a2c2441a064e76f73d7b84b
Atul Singh <cocatul11@gmail.com>
2024-08-06 20:48:18 +0530

    feat: Add StreetLightNotFoundException class

---
+++ Street_light/src/main/java/ashis/com/backend/Street_light/exceptions/StreetLightNotFoundException.java
@@ -0,0 +1,7 @@
+package ashis.com.backend.Street_light.exceptions;
+
+public class StreetLightNotFoundException extends RuntimeException{
+    public StreetLightNotFoundException(String message) {
+        super(message);
+    }
+}
