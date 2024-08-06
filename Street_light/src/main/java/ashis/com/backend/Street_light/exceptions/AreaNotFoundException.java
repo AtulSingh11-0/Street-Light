4d9e8f9f9dd1aafa0f81f3da41bf5ae7bf96772b
Atul Singh <cocatul11@gmail.com>
2024-08-06 20:48:22 +0530

    feat: Add AreaNotFoundException class

---
+++ Street_light/src/main/java/ashis/com/backend/Street_light/exceptions/AreaNotFoundException.java
@@ -0,0 +1,7 @@
+package ashis.com.backend.Street_light.exceptions;
+
+public class AreaNotFoundException extends RuntimeException{
+  public AreaNotFoundException ( String message){
+    super(message);
+  }
+}
